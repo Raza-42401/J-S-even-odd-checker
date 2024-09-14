8d01af904179670a55bfe33119b3a7bd1e07e5ca
Raza-42401 <officialkashaafraza@gmail.com>
2024-09-14 10:47:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let yourNumber = Number(prompt("Enter a Number"));
+if (yourNumber % 2  === 0){
+    alert("the Number is even")
+}else{
+    alert("the number is odd")
+}
+let increment = ++yourNumber
+alert("after increment the number is: " + increment);
